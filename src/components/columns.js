d40a2da9ee78afd26e43d0fdabb894751e1e4116
olaronaldo <83012662+olaronaldo@users.noreply.github.com>
2022-04-07 17:19:26 +0100

    second commit

---
+++ src/components/columns.js
@@ -0,0 +1,26 @@
+export const COLUMNS = [
+    {
+        Header: 'Id',
+        accessor: 'id'
+    }, {
+        Header: 'Full Name',
+        accessor:'full_name'
+    },
+    {
+        Header: 'Email',
+        accessor:'email'
+    },
+    {
+        Header: 'Gender',
+        accessor:'gender'
+    },
+    {
+        Header: 'Age',
+        accessor:'age'
+    },
+    {
+        Header: 'Date Submitted',
+        accessor: 'date_submitted'
+    },
+
+]
